7aba955ddbbf22e179e9411023b68aabd0434cbf
angel4uu <angela.jara@unmsm.edu.pe>
2025-10-10 20:53:58 -0500

    add initial migrations

---
+++ Migrations/20251010163642_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SearchMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "historial_busqueda",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    id_usuario = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    texto_busqueda = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    fecha_busqueda = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "NOW()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_historial_busqueda", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "historial_busqueda");
+        }
+    }
+}
